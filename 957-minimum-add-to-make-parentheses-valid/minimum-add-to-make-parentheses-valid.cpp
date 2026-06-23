0889764d6936b4990823c5c640ac5fdf3471dc04
Pushkar Agarwal <agrawalpushkar83@gmail.com>
2026-06-23 20:26:16 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (30.86%) - LeetSync

--- 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.cpp
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     int minAddToMakeValid(string s) {
-       stack<char>st;
-       int ans=0;
-       for(int i=0;i<s.length();i++){
-        if(s[i]=='('){st.push(s[i]);}
-        else{
-            if(st.empty()){ans++;}
-            else
-                st.pop();
-            
+        int open = 0;
+        int close = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '(')
+                open++;
+            else {
+                if (open > 0)
+                    open--;
+                else {
+                    close++;
+                }
+            }
         }
-       }
-       return ans+st.size();
+        return open + close;
     }
 };
