c15ccc3f974c1acab8050ef74a124395e190e7f0
Pushkar Agarwal <agrawalpushkar83@gmail.com>
2026-07-15 16:02:44 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (85.02%) - LeetSync

---
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        // int sumeven=n*n;
+        // int sumodd=n*(n+1);
+        // return gcd(sumeven,sumodd);
+        return n;
+    }
+};
