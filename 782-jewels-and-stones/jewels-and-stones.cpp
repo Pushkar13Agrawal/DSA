82c3c8db8377a94251b9a70ad55b4a5dee73ca8e
Pushkar Agarwal <agrawalpushkar83@gmail.com>
2026-08-24 00:48:03 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (53.50%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.cpp
+++ 782-jewels-and-stones/jewels-and-stones.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     int numJewelsInStones(string jewels, string stones) {
         map<char, int> mpp;
-        for (int i = 0; i < stones.size(); i++) {
-            mpp[stones[i]]++;
+        for (int i = 0; i < jewels.size(); i++) {
+            mpp[jewels[i]]++;
         }
         int ans = 0;
-        for (int i = 0; i < jewels.size(); i++) {
-            if (mpp.find(jewels[i]) != mpp.end()) {
-                ans = ans + mpp[jewels[i]];
+        for (int i = 0; i < stones.size(); i++) {
+            if (mpp.find(stones[i]) != mpp.end()) {
+                ans++;
             }
         }
         return ans;
